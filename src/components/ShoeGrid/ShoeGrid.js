2e6e7045d8b12b7504ceaa9230d7ceda8b71ecff
Phillip Truong <ptruong@konradgroup.com>
2022-07-26 09:41:32 -0400

    updated shoe grid and card

--- src/components/ShoeGrid/ShoeGrid.js
+++ src/components/ShoeGrid/ShoeGrid.js
@@ -1,8 +1,7 @@
-import React from 'react';
-import styled from 'styled-components/macro';
+import styled from "styled-components/macro";
 
-import SHOES from '../../data';
-import ShoeCard from '../ShoeCard';
+import SHOES from "../../data";
+import ShoeCard from "../ShoeCard";
 
 const ShoeGrid = () => {
   return (
@@ -14,6 +13,10 @@ const ShoeGrid = () => {
   );
 };
 
-const Wrapper = styled.div``;
+const Wrapper = styled.div`
+  display: flex;
+  gap: 36px;
+  flex-wrap: wrap;
+`;
 
 export default ShoeGrid;
